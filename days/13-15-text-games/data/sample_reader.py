d58f3e9918ccd7ff82d7b9126546728e2dfbbc10
Russell Helmstedter <rhelmstedter@gmail.com>
2021-06-08 15:49:04 -0700

    rock paper scissors

--- days/13-15-text-games/data/sample_reader.py
+++ days/13-15-text-games/data/sample_reader.py
@@ -1,22 +1,24 @@
-import csv 
+import csv
+
 
 def read_rolls():
-    with open('battle-table.csv') as fin:
+    with open("battle-table.csv") as fin:
         reader = csv.DictReader(fin)
         for row in reader:
             read_roll(row)
 
 
 def read_roll(row: dict):
-    name = row['Attacker']
-    del row['Attacker']
+    name = row["Attacker"]
+    del row["Attacker"]
     del row[name]
 
     print("Roll: {}".format(name))
     for k in row.keys():
-        can_defeat = row[k].strip().lower() == 'win'
+        can_defeat = row[k].strip().lower() == "win"
         print(" * {} will defeat {}? {}".format(name, k, can_defeat))
 
     print()
+
 
 read_rolls()
--- days/13-15-text-games/dnd_game/program.py
+++ days/13-15-text-games/dnd_game/program.py
@@ -1,53 +1,58 @@
 from actors import Creature, Wizard, Dragon
 import random
 
+
 def main():
     print_header()
     game_loop()
 
 
 def print_header():
-    print('---------------------------------')
-    print('          WIZARD GAME')
-    print('---------------------------------')
+    print("---------------------------------")
+    print("          WIZARD GAME")
+    print("---------------------------------")
     print()
 
 
 def game_loop():
     creatures = [
-        Creature('Bat', 5),
-        Creature('Toad', 1),
-        Creature('Tiger', 12),
-        Dragon('Black Dragon', 50, scaliness=2, breaths_fire=False),
-        Wizard('Evil wizard', 1000),
+        Creature("Bat", 5),
+        Creature("Toad", 1),
+        Creature("Tiger", 12),
+        Dragon("Black Dragon", 50, scaliness=2, breaths_fire=False),
+        Wizard("Evil wizard", 1000),
     ]
 
-    hero = Wizard('Gandolf', 75)
+    hero = Wizard("Gandolf", 75)
 
     while True:
 
         active_creature = random.choice(creatures)
 
-        print('A {} of level {} has appear from a dark and foggy forest...'
-              .format(active_creature.name, active_creature.level))
+        print(
+            "A {} of level {} has appear from a dark and foggy forest...".format(
+                active_creature.name, active_creature.level
+            )
+        )
         print()
 
-        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
-        if cmd == 'a':
+        cmd = input("Do you [a]ttack, [r]unaway, or [l]ook around? ")
+        if cmd == "a":
             if hero.attack(active_creature):
                 creatures.remove(active_creature)
                 print("The wizard defeated {}".format(active_creature.name))
             else:
-                print("The wizard has been defeat by the powerful {}".format(active_creature.name))
-        elif cmd == 'r':
-            print('The wizard has become unsure of his power and flees!!!')
-        elif cmd == 'l':
-            print('The wizard {} takes in the surroundings and sees:'
-                  .format(hero.name))
+                print(
+                    "The wizard has been defeat by the powerful {}".format(
+                        active_creature.name
+                    )
+                )
+        elif cmd == "r":
+            print("The wizard has become unsure of his power and flees!!!")
+        elif cmd == "l":
+            print("The wizard {} takes in the surroundings and sees:".format(hero.name))
             for c in creatures:
-                print(" * {} of level {}".format(
-                    c.name, c.level
-                ))
+                print(" * {} of level {}".format(c.name, c.level))
         else:
             print("OK, exiting game... bye!")
             break
@@ -59,5 +64,5 @@ def game_loop():
         print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
